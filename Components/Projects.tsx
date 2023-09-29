388b23b248678d9c98be706304bcf6253e656b79
Maria del Mar <chocoinformatics2016@gmail.com>
2023-09-29 11:31:10 +0200

    everything

--- Components/Projects.tsx
+++ Components/Projects.tsx
@@ -3,9 +3,24 @@ import React from "react";
 import ProjectCard from "./ProjectCard";
 import projectData from "../public/Data/Projects";
 import ProjectTabButton from "./ProjectTabButton";
-import { useState, useTransition } from "react";
+import { useState, useTransition, useRef } from "react";
+import { motion, useInView } from "framer-motion";
 
 function Projects() {
+  // this is for using the animation, if the object that I want to animate is not going to be visible in the first reload of the website. Then we need useRef (from react) and useInView (from motion) in order to set from when we want to see the animation
+
+  const ref = useRef(null);
+
+  //so this define a referece on the page, and once this point of reference is in the view of the user after scrolling, then the effect takes place.
+  const isInView = useInView(ref, { once: true });
+
+  // for the animation, we can also define states, since we want the cards to appear one after the other.
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
   const [tag, setTag] = useState("All");
   const handleTagChange = (newTag: string) => {
     setTag(newTag);
@@ -14,7 +29,7 @@ function Projects() {
     return project.tag.includes(tag);
   });
   return (
-    <>
+    <section>
       <h2 className="text-left md:text-center text-4xl font-bold text-white mt-4 mb-4 md:mb-8 ">
         My Projects
       </h2>
@@ -36,22 +51,31 @@ function Projects() {
           isSelected={tag === "Web2"}
         />
       </div>
-      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+      {/* from here we want our ref to take place */}
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
         {/* here we just map the result from the filter function we wrote above to look for the ones that have in the tag array the tag that we are selecting */}
-        {filteredProjects.map((project) => {
+        {filteredProjects.map((project, index) => {
           return (
-            <ProjectCard
+            // so first, we pass cardVariants as a prop for the parameter variants, then initial, we choose the first state that is inside of the animation that we described in the cardVariants, and then in animate, we only pass the animation inside the cardVariants when isInview is true. each project will be delayed by whatever the index is, multiply by the number (this is custumazible)
+            <motion.li
               key={project.id}
-              imgUrl={project.image}
-              description={project.description}
-              title={project.title}
-              gitUrl={project.gitUrl}
-              previewUrl={project.previewUrl}
-            />
+              variants={cardVariants}
+              initial="initial"
+              animate={isInView ? "animate" : "initial"}
+              transition={{ duration: 0.3, delay: index * 0.4 }}
+            >
+              <ProjectCard
+                imgUrl={project.image}
+                description={project.description}
+                title={project.title}
+                gitUrl={project.gitUrl}
+                previewUrl={project.previewUrl}
+              />
+            </motion.li>
           );
         })}
-      </div>
-    </>
+      </ul>
+    </section>
   );
 }
 
--- Components/TabButton.tsx
+++ Components/TabButton.tsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+const tabVariants = {
+  default: { width: 0 },
+  active: { width: "calc(100% - 0.75rem)" },
+};
 
 interface TabButtonsProps {
   active: boolean;
@@ -11,14 +17,18 @@ const TabButton: React.FC<TabButtonsProps> = ({
   selectTab,
   children,
 }) => {
-  const buttonClasses = active
-    ? "text-white border-b border-primary-500"
-    : "text-[#ADB7BE] ";
+  const buttonClasses = active ? "text-white" : "text-[#ADB7BE] ";
   return (
     <button onClick={selectTab}>
       <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
         {children}
       </p>
+      {/* we cannot animate the border, but we can create a div that has a border */}
+      <motion.div
+        animate={active ? "active" : "default"}
+        variants={tabVariants}
+        className="h-1 bg-primary-500 mt-2 mr-3"
+      ></motion.div>
     </button>
   );
 };
